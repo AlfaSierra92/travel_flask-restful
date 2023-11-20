3aaad2df6366843898245ed743013e57514ab632
Antonio Solida <solida.antonio@gmail.com>
2023-11-20 12:15:36 +0100

    Refactored

--- travel_gcloud.py
+++ travel_gcloud.py
@@ -1,6 +1,6 @@
 import json
 from google.cloud import firestore
-
+from datetime import datetime
 
 class Travel(object):
     def __init__(self):
@@ -28,6 +28,26 @@ class Travel(object):
             return None
 
     def get_list(self, name):
+        travelmates = []
+
+        # forse il .get() lo devi fare fuori dai costrutti if e for ???
+        travel_documents = self.db.collection('travel').get()
+
+        for doc in travel_documents:
+            if name.lower() in doc.get("name"):
+                for x in travel_documents:
+                    data1 = datetime.strptime(x.get("date"), "%Y-%m-%d")
+                    data2 = datetime.strptime(doc.get("date"), "%Y-%m-%d")
+                    diff = data1-data2
+                    if (
+                            (data1 == data2 or abs(diff.days) <= 1)
+                            and x.get("departure") == doc.get("departure")
+                            and x.get("arrive") == doc.get("arrive")):
+                        travelmates.append(x.get("name"))
+        return travelmates
+
+    # DEPRECATED
+    def get_list_old(self, name):
         travelmates = []
 
         # Get all documents in the 'travel' collection
